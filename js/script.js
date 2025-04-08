e140d3db89a2e8041aa0e393f4dfedd54cda4f41
Felipe Rosas <felipe.rosas1199@gmail.com>
2025-04-08 12:50:16 -0600

    Pruebas con beforebeign y afterend JS

--- js/script.js
+++ js/script.js
@@ -37,9 +37,27 @@ btnMostrar.addEventListener("click",function(event) {
 
     let element2 = element.cloneNode(true);
     
+    // Before inserta el elemento antes de la lista 
     // lista.item(0).before(element);
+    // Prepend inserta el elemento al icnio de la lista uwu 
     // lista.item(0).prepend(element2);
 
-    lista.item(0).append(element);
-    lista.item(0).after(element2);
+    // Appne dinserta el lemento al final de la lista 
+    // lista.item(0).append(element);
+    // after inserta el elemento despu es de la lista
+    // lista.item(0).after(element2);
+
+    // afterbeginong
+
+    // // afterbegin inserta el elemento al princio de la lista 
+    // lista.item(1).insertAdjacentElement("afterbegin",element)
+    // // beforeend inserta el lemento al final de la lista
+    // lista.item(1).insertAdjacentElement("beforeend", element2)
+
+    lista.item(1).insertAdjacentHTML("beforebegin",`<li class="list-group-item p-4 active" >Aother new item</li>`)
+    lista.item(1).insertAdjacentHTML("afterend",`<li class="list-group-item p-4 active" >Aother new afterend item last item</li>`)
+
+
+    lista.item(1).insertAdjacentHTML("afterbegin",`<li class="list-group-item p-4 active" >Aother new item</li>`)
+    lista.item(1).insertAdjacentHTML("beforeend",`<li class="list-group-item p-4 active" >Aother beforened item last item</li>`)
 });
